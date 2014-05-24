79a48cb4d376e02369515145ca518b511dd37897
Erich Hauptli <ehauptli@utexas.edu>
2014-05-23 20:42:48 -0500

    Moved to SQL Package.  Added ID.

--- src/sql/DownloadFromDB.java
+++ src/sql/DownloadFromDB.java
@@ -1,4 +1,4 @@
-package user;
+package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,18 +10,26 @@ public class DownloadFromDB {
 	public static void main(String[] args) throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:sqlite:user.db");
         Statement stat = null;
-        String query = "SELECT * FROM user WHERE education LIKE 'MSU'";
+        //String query = "SELECT * FROM user WHERE education LIKE 'MSU'";
+        String query = "SELECT * FROM user";
+        //String size = "SELECT COUNT(*) AS count FROM user";
 
         try {
             stat = conn.createStatement();
+            //Statement stmt1 = conn.createStatement();
             ResultSet rs = stat.executeQuery(query);
+            //ResultSet rs1 = stmt1.executeQuery(size);
+            //int count = rs1.getInt("count");
+            
             while (rs.next()) {
+            	System.out.println("id = " + rs.getString("id"));
                 System.out.println("name = " + rs.getString("name"));
                 System.out.println("birthday = " + rs.getString("birthday"));
                 System.out.println("education = " + rs.getString("education"));
                 System.out.println("work = " + rs.getString("work"));
                 System.out.println("\n");
             }
+            //System.out.println(count);
         } catch (SQLException e ) {
         } finally {
             if (stat != null) { stat.close(); }
--- src/sql/UploadToDB.java
+++ src/sql/UploadToDB.java
@@ -1,4 +1,4 @@
-package user;
+package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,19 +7,29 @@ import java.sql.Statement;
 
 public class UploadToDB {
     public static void main(String[] args) throws Exception {
+    	
+    	//String size = "SELECT COUNT(*) AS count FROM user";
+    	    	
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:user.db");
-        Statement stat = conn.createStatement();
+        Statement stmt = conn.createStatement();
+        //Statement stmt1 = conn.createStatement();
         //stat.executeUpdate("drop table if exists user;");			//Remove table if it exists
-        stat.executeUpdate("create table if not exists user(name, birthday, education, work);");   //Create table
+        stmt.executeUpdate("create table if not exists user(id, name, birthday, education, work);");   //Create table
+        
+        //ResultSet rs1 = stmt1.executeQuery(size);
+        //int count = rs1.getInt("count");
+        //System.out.println(count);
+        
         //stat.executeUpdate("insert table user(name, birthday, education, work);");
         PreparedStatement prep = conn.prepareStatement(
-            "insert into user values (?, ?, ?, ?);");
+            "insert into user values (?, ?, ?, ?, ?);");
         
-        prep.setString(1, "Dan Teller");
-        prep.setString(2, "8/11/1986");
-        prep.setString(3, "");
-        prep.setString(4, "Excavator");
+        prep.setString(1, "1");
+        prep.setString(2, "Erich Hauptli");
+        prep.setString(3, "8/30/1982");
+        prep.setString(4, "MSU");
+        prep.setString(5, "Engineer");
         prep.addBatch();
 		
         conn.setAutoCommit(false);
