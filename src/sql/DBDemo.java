0b9942e5a50d75ebd4e339b0245cf727bddd820b
Erich Hauptli <ehauptli@utexas.edu>
2014-05-24 21:48:50 -0500

    Used file to upload data to database.

--- src/sql/DBDemo.java
+++ src/sql/DBDemo.java
@@ -1,28 +1,29 @@
 package sql;
 
+import java.io.IOException;
+
+import files.ReadFile;
+
 public class DBDemo {
     public static void main(String[] args) throws Exception {
 
-        // Create two different 
-        // User objects
-   	
-    	String[] erich = {"1", "Erich Hauptli", "August 30, 1982", "MSU", "Engineer"};
-    	String[] jessica = {"2","Jessica Hauptli", "April 8, 1983", "MSU", "Mom"};
-    	String[] lukas = {"3","Lukas Hauptli", "June 23, 2012", "NA", "Kiddo"};
-    	String[] leslie = {"4","Leslie Hauptli", "April 6, 1955", "MSU", "Banker"};
-    	String[] jurg = {"5","Jurg Hauptli", "April 25, 1955", "MSU", "Engineer"};
-    	String[] krista = {"6","Krista Teller", "September 13, 1984", "OAK", "Analyst"};
-    	String[] dan = {"7","Dan Teller", "August 11, 1986", "NA", "Excavator"};
-
-        // Invoke methods on 
-        // those objects
-    	UploadToDB.upload(erich);
-    	UploadToDB.upload(jessica);
-    	UploadToDB.upload(lukas);
-    	UploadToDB.upload(leslie);
-    	UploadToDB.upload(jurg);
-    	UploadToDB.upload(krista);
-    	UploadToDB.upload(dan);
+    	
+		String file_name = "people.csv";
+		
+		try{
+			ReadFile file = new ReadFile(file_name);
+			String[] aryLines = file.OpenFile();
+			
+			int i;
+			for(i=0; i<aryLines.length; i++){
+				String line = aryLines[i];
+				String[] split = line.split("\\s*,\\s*");
+				UploadToDB.upload(split);
+			}
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+		}
     	
     	DownloadFromDB.download(null);
     }
